9656370fe035127f2e7848f2935856ca2ba1a1d5
vikrant78951 <vikrant78951@gmail.com>
2025-05-21 01:39:09 +0530

    theme configured

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Link from 'next/link'
+import { Button } from '@/src/components/ui/button'
  
 export default function Page() {
   return (
     <div>
       <h1>Home</h1>
       <Link href="/about">About</Link>
+      <Button variant={'default'}>some shit</Button>
     </div>
   )
 }
